963e23507f26122388455bd100a56bb5e23cc39d
Suryapratap Rathore <suryapratap.rathore@gmail.com>
2024-09-12 23:38:02 +0530

    prevent root catchall

--- main.go
+++ main.go
@@ -18,12 +18,12 @@ func snippetCreate(w http.ResponseWriter, r *http.Request){
 }
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home) // restrict this route
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting server on :4000")
-	
+
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
