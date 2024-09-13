89af9d026c9cbd280aae99b08f89ae3a37a7cd03
Suryapratap Rathore <suryapratap.rathore@gmail.com>
2024-09-13 07:30:25 +0530

    added create post route

--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
-	"fmt"
-
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request){
+func snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -22,14 +21,20 @@ func snippetView(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte(msg))
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request){
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a form for creating a new snippet..."))
 }
+
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("save a new snippet"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /{$}", home) // restrict this route
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
 	log.Print("starting server on :4000")
 
