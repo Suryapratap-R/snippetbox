bdbf79eed33d210fe5524cb7e6c0bf20b67fd542
Suryapratap Rathore <suryapratap.rathore@gmail.com>
2024-09-19 21:10:46 +0530

    view call

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 	"strconv"
-	"html/template"
+
+	"snippetbox.heysurya.com/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -15,13 +18,13 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		"./ui/html/partials/nav.tmpl.html",
 		"./ui/html/pages/home.tmpl.html",
 	}
-	ts, err:= template.ParseFiles(files...)
-	if err != nil{
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
 	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil{
+	if err != nil {
 		app.serverError(w, r, err)
 	}
 }
@@ -33,7 +36,17 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.NotFound(w, r)
+		} else {
+			app.serverError(w, r, err)
+		}
+		return
+	}
+
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -41,6 +54,15 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("Save a new snippet..."))
+	title := "bla title"
+	content := "bla bal \nmount everaasdfj\nkadsfjad"
+	expires := 7
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
+	// w.WriteHeader(http.StatusCreated)
+	// w.Write([]byte("Save a new snippet..."))
 }
